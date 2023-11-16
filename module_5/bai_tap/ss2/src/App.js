94c9a3039b1003b25e086c0149839016829e90df
TUẤN <tranvanlamqbdl@gmail.com>
2023-11-16 09:01:03 +0700

    fix comment

--- module_5/bai_tap/ss2/src/App.js
+++ module_5/bai_tap/ss2/src/App.js
@@ -1,50 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
-const students = [
-  {
-    company: 'Alfreds Futterkiste',
-    contact: 'Maria Anders',
-    country: 'Germany'
-  },
-  {
-    company: 'Centro comercial Moctezuma',
-    contact: 'Francisco Chang',
-    country: 'Mexico'
-  },
-  {
-    company: 'Ernst Handel',
-    contact: 'Roland Mendel',
-    country: 'Austria'
-  },
-  {
-    company: 'Island Trading',
-    contact: 'Helen Bennett',
-    country: 'UK'
-  },
-  {
-    company: 'Laughing Bacchus Winecellars',
-    contact: 'Yoshi Tannamuri',
-    country: 'Canada'
-  },
-  {
-    company: 'Magazzini Alimentari Riuniti',
-    contact: 'Giovanni Rovelli',
-    country: 'Italy'
-  }
-]
+
 
 function App() {
   return (
-        <>
-        {students.map(student => (
-          <tr>
-            <td>{student.company}</td>
-            <td>{student.contact}</td>
-            <td>{student.country}</td>
-          </tr>
-        ))}
-    </>
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
   );
 }
 
--- module_5/bai_tap/ss2/src/index.js
+++ module_5/bai_tap/ss2/src/index.js
@@ -4,10 +4,57 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const students = [
+  {
+    id : 1,
+    company: 'Alfreds Futterkiste',
+    contact: 'Maria Anders',
+    country: 'Germany'
+  },
+  {
+    id : 2,
+    company: 'Centro comercial Moctezuma',
+    contact: 'Francisco Chang',
+    country: 'Mexico'
+  },
+  {
+    id : 3,
+    company: 'Ernst Handel',
+    contact: 'Roland Mendel',
+    country: 'Austria'
+  },
+  {
+    id : 4,
+    company: 'Island Trading',
+    contact: 'Helen Bennett',
+    country: 'UK'
+  },
+  {
+    id : 5,
+    company: 'Laughing Bacchus Winecellars',
+    contact: 'Yoshi Tannamuri',
+    country: 'Canada'
+  },
+  {
+    id : 6,
+    company: 'Magazzini Alimentari Riuniti',
+    contact: 'Giovanni Rovelli',
+    country: 'Italy'
+  }
+]
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+    {students.map(student => (
+          <tr key={student.id}>
+            <td>{student.company}</td>
+            <td>{student.contact}</td>
+            <td>{student.country}</td>
+          </tr>
+        ))}
+        </>
   </React.StrictMode>
 );
 
