22aac276b60662741ce9bdd472d8818369557fd3
TUẤN <tranvanlamqbdl@gmail.com>
2023-11-16 09:20:39 +0700

    fix comment

--- module_5/bai_tap/ss2/src/index.js
+++ module_5/bai_tap/ss2/src/index.js
@@ -3,58 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Student } from './components/student';
 
-const students = [
-  {
-    id : 1,
-    company: 'Alfreds Futterkiste',
-    contact: 'Maria Anders',
-    country: 'Germany'
-  },
-  {
-    id : 2,
-    company: 'Centro comercial Moctezuma',
-    contact: 'Francisco Chang',
-    country: 'Mexico'
-  },
-  {
-    id : 3,
-    company: 'Ernst Handel',
-    contact: 'Roland Mendel',
-    country: 'Austria'
-  },
-  {
-    id : 4,
-    company: 'Island Trading',
-    contact: 'Helen Bennett',
-    country: 'UK'
-  },
-  {
-    id : 5,
-    company: 'Laughing Bacchus Winecellars',
-    contact: 'Yoshi Tannamuri',
-    country: 'Canada'
-  },
-  {
-    id : 6,
-    company: 'Magazzini Alimentari Riuniti',
-    contact: 'Giovanni Rovelli',
-    country: 'Italy'
-  }
-]
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <>
-    {students.map(student => (
-          <tr key={student.id}>
-            <td>{student.company}</td>
-            <td>{student.contact}</td>
-            <td>{student.country}</td>
-          </tr>
-        ))}
-        </>
+    < Student />
   </React.StrictMode>
 );
 
