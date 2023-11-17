be83fcb0e05f741d1e68614b37a92de28d2c21f4
TUẤN <tranvanlamqbdl@gmail.com>
2023-11-17 16:06:03 +0700

    case study

---
+++ module_5/bai_tap/counter/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import UseIncrement from './components/UseIncrement';
+
+function App() {
+  return (
+    <>
+     < UseIncrement />
+    </>
+   
+  );
+}
+
+export default App;
